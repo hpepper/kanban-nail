c7fbedd7f176531ac9586a4086304b6c170ec41f
hp <hp@spam.no>
2015-09-16 21:28:44 +0200

    Added Juce start project.

---
+++ KanBanNail/Builds/Android/jni/Android.mk
@@ -0,0 +1,42 @@
+# Automatically generated makefile, created by the Introjucer
+# Don't edit this file! Your changes will be overwritten when you re-save the Introjucer project!
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+    LOCAL_ARM_MODE := arm
+endif
+
+LOCAL_MODULE := juce_jni
+LOCAL_SRC_FILES := \
+  ../../../Source/MainComponent.cpp\
+  ../../../Source/Main.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_audio_basics/juce_audio_basics.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_audio_devices/juce_audio_devices.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_audio_formats/juce_audio_formats.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_audio_processors/juce_audio_processors.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_core/juce_core.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_cryptography/juce_cryptography.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_data_structures/juce_data_structures.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_events/juce_events.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_graphics/juce_graphics.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_gui_basics/juce_gui_basics.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_gui_extra/juce_gui_extra.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_opengl/juce_opengl.cpp\
+  ../../../../../../../../../opt/juce/modules/juce_video/juce_video.cpp\
+
+ifeq ($(NDK_DEBUG),1)
+  LOCAL_CPPFLAGS += -fsigned-char -fexceptions -frtti -g -I "../../JuceLibraryCode" -I "../../../../../../../../opt/juce/modules" -O0 -std=c++11 -std=gnu++11 -D "JUCE_ANDROID=1" -D "JUCE_ANDROID_API_VERSION=10" -D "JUCE_ANDROID_ACTIVITY_CLASSNAME=com_yourcompany_kanbannail_KanBanNail" -D JUCE_ANDROID_ACTIVITY_CLASSPATH=\"com/yourcompany/kanbannail/KanBanNail\" -D "DEBUG=1" -D "_DEBUG=1" -D "JUCER_ANDROID_7F0E4A25=1" -D "JUCE_APP_VERSION=0.1.0" -D "JUCE_APP_VERSION_HEX=0x100"
+  LOCAL_LDLIBS := -llog -lGLESv2
+  LOCAL_CFLAGS += -fsigned-char -fexceptions -frtti -g -I "../../JuceLibraryCode" -I "../../../../../../../../opt/juce/modules" -O0 -std=c++11 -std=gnu++11 -D "JUCE_ANDROID=1" -D "JUCE_ANDROID_API_VERSION=10" -D "JUCE_ANDROID_ACTIVITY_CLASSNAME=com_yourcompany_kanbannail_KanBanNail" -D JUCE_ANDROID_ACTIVITY_CLASSPATH=\"com/yourcompany/kanbannail/KanBanNail\" -D "DEBUG=1" -D "_DEBUG=1" -D "JUCER_ANDROID_7F0E4A25=1" -D "JUCE_APP_VERSION=0.1.0" -D "JUCE_APP_VERSION_HEX=0x100"
+  LOCAL_LDLIBS := -llog -lGLESv2
+else
+  LOCAL_CPPFLAGS += -fsigned-char -fexceptions -frtti -I "../../JuceLibraryCode" -I "../../../../../../../../opt/juce/modules" -O3 -std=c++11 -std=gnu++11 -D "JUCE_ANDROID=1" -D "JUCE_ANDROID_API_VERSION=10" -D "JUCE_ANDROID_ACTIVITY_CLASSNAME=com_yourcompany_kanbannail_KanBanNail" -D JUCE_ANDROID_ACTIVITY_CLASSPATH=\"com/yourcompany/kanbannail/KanBanNail\" -D "NDEBUG=1" -D "JUCER_ANDROID_7F0E4A25=1" -D "JUCE_APP_VERSION=0.1.0" -D "JUCE_APP_VERSION_HEX=0x100"
+  LOCAL_LDLIBS := -llog -lGLESv2
+  LOCAL_CFLAGS += -fsigned-char -fexceptions -frtti -I "../../JuceLibraryCode" -I "../../../../../../../../opt/juce/modules" -O3 -std=c++11 -std=gnu++11 -D "JUCE_ANDROID=1" -D "JUCE_ANDROID_API_VERSION=10" -D "JUCE_ANDROID_ACTIVITY_CLASSNAME=com_yourcompany_kanbannail_KanBanNail" -D JUCE_ANDROID_ACTIVITY_CLASSPATH=\"com/yourcompany/kanbannail/KanBanNail\" -D "NDEBUG=1" -D "JUCER_ANDROID_7F0E4A25=1" -D "JUCE_APP_VERSION=0.1.0" -D "JUCE_APP_VERSION_HEX=0x100"
+  LOCAL_LDLIBS := -llog -lGLESv2
+endif
+
+include $(BUILD_SHARED_LIBRARY)
